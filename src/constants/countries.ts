a5564f35be6f329d4f8915525afb3aaf9bf12f31
Legalfiction <aldo.huizinga@gmail.com>
2026-02-20 21:22:02 +0100

    Create countries.ts

---
+++ src/constants/countries.ts
@@ -0,0 +1,12 @@
+export const COUNTRIES: any = {
+  NL: { flag: '🇳🇱', prefix: '+31', name: 'Nederland', lang: 'nl', days: ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag'] },
+  BE: { flag: '🇧🇪', prefix: '+32', name: 'België', lang: 'nl', days: ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag'] },
+  UK: { flag: '🇬🇧', prefix: '+44', name: 'United Kingdom', lang: 'en', days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] },
+  US: { flag: '🇺🇸', prefix: '+1', name: 'United States', lang: 'en', days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] },
+  DE: { flag: '🇩🇪', prefix: '+49', name: 'Deutschland', lang: 'de', days: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'] },
+  FR: { flag: '🇫🇷', prefix: '+33', name: 'France', lang: 'fr', days: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'] },
+  ES: { flag: '🇪🇸', prefix: '+34', name: 'España', lang: 'es', days: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'] },
+  IT: { flag: '🇮🇹', prefix: '+39', name: 'Italia', lang: 'it', days: ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica'] },
+  PL: { flag: '🇵🇱', prefix: '+48', name: 'Polska', lang: 'pl', days: ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'] },
+  TR: { flag: '🇹🇷', prefix: '+90', name: 'Türkiye', lang: 'tr', days: ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'] }
+};
