2c627b36b6dbc05eb84b2ad2405b91b8c0182917
Legalfiction <aldo.huizinga@gmail.com>
2026-02-21 10:24:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Genereer een unieke tijdstempel tijdens het bouwen
+const timestamp = Date.now();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +11,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        // Forceert de browser om bij een update ALTIJD de nieuwe versie te downloaden
+        entryFileNames: `assets/[name]-[hash]-v${timestamp}.js`,
+        chunkFileNames: `assets/[name]-[hash]-v${timestamp}.js`,
+        assetFileNames: `assets/[name]-[hash]-v${timestamp}.[ext]`
+      }
+    }
   },
   server: {
     host: true
